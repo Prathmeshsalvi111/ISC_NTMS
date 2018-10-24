a9457dd6752242ea479f30d407c6a98154206e26
Prathmeshsalvi111 <38372049+Prathmeshsalvi111@users.noreply.github.com>
2018-10-24 07:40:42 -0700

    NTMS Task 24-10-2018
    NTMS Task 24-10-2018

---
+++ prathmesh.ps1
@@ -0,0 +1,10 @@
+﻿# Identify a cmdlet that produces a random number
+Get-Random
+#Identify a cmdlet that displays the current dat and time
+Get-Date
+#What type of object the cmdlet from task 2 Produce?
+# dont no this
+#Identify the cmdlet that displays information about installed hotfixes on windows
+Get-HotFix
+#Display a list of 50 newest entries from security log.
+Get-EventLog -LogName Security -Newest 50 
