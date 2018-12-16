2a1729978fb1edf95b05bbce160d22d2050650e3
Prathmeshsalvi111 <38372049+Prathmeshsalvi111@users.noreply.github.com>
2018-12-16 19:21:28 +0530

    check windows patch on Remote_computer
    check windows patch on Remote_computer

---
+++ windows patch check on remote computer.ps1
@@ -0,0 +1,14 @@
+﻿$Serverlist = Import-Csv -Path C:\Users\citrixadmin\Desktop\Book1.csv
+$patch_no = read-host -Prompt "provide patch no"
+write-host "$patch_no check the patch"
+if($patch_no -match  "^[K]b\d{7}$")
+	{ Write-Host "validating the patch from remote computer"
+           foreach($server in $Serverlist.hostname) 
+                    {Get-wmiobject -class win32_quickfixengineering -ComputerName $server -ErrorAction SilentlyContinue | Where-Object{$_.HotFixID -match  "$patch_no"}}
+                    if($patch_no -match $patch_no)
+                        {write-host "patch not available"}
+                        else { write-host "patch is available remote matchine"}}
+     elseif ($patch_no -match  "^[K]b\d{7}$")
+                 { write-host "please provide proper Character" }
+                        else { write-host "Wrong input"}
+                           
